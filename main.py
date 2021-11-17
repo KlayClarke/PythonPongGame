0b4ec44864b8bfd797cc7347b411fa19fda3ddb9
Klay Clarke <klayaclarke@gmail.com>
2021-11-17 13:45:46 -0500

    add functionality for moving user paddle up and down using corresponding keys on keyboard

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from turtle import Turtle, Screen
+from computer_paddle import ComputerPaddle
+from user_paddle import UserPaddle
+
+
+game_on = True
+
+cpu_paddle = ComputerPaddle()
+user_paddle = UserPaddle()
+
+print(user_paddle.xcor())
+print(user_paddle.ycor())
 
 screen = Screen()
+screen.listen()
+screen.onkey(user_paddle.go_up, 'Up')
+screen.onkey(user_paddle.go_down, 'Down')
+screen.title('Pong')
 screen.bgcolor('black')
 screen.setup(width=800, height=600)
+
+while game_on:
+    screen.update()
+
+
 screen.exitonclick()
