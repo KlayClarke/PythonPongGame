807bd69596426daa1f2581f1fcad72cbe258dced
Klay Clarke <klayaclarke@gmail.com>
2021-11-17 21:03:40 -0500

    create paddle methods for up and down movement along the y axis

--- paddle.py
+++ paddle.py
@@ -4,7 +4,16 @@ from turtle import Turtle
 class Paddle(Turtle):
     def __init__(self):
         super().__init__()
+        self.speed('fastest')
         self.penup()
         self.shape('square')
-        self.shapesize(stretch_wid=5, stretch_len=1, outline=None)
+        self.shapesize(stretch_wid=5, stretch_len=0.5, outline=None)
         self.color('white')
+
+    def go_up(self):
+        new_y_position = self.ycor() + 10
+        self.goto(x=self.xcor(), y=new_y_position)
+
+    def go_down(self):
+        new_y_position = self.ycor() - 10
+        self.goto(x=self.xcor(), y=new_y_position)
