282861dbeee25c3e2756fc9d36adf2458c3650a1
Klay Clarke <klayaclarke@gmail.com>
2021-11-17 21:49:38 -0500

    change file name

--- left_paddle.py
+++ left_paddle.py
@@ -4,7 +4,7 @@ x_position = -350
 y_position = 0
 
 
-class UserPaddle(Paddle):
+class LeftPaddle(Paddle):
     def __init__(self):
         super().__init__()
         self.goto(x=x_position, y=y_position)
