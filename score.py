43cd9e0d9e12c2daec790f7217721324b323e22d
Klay Clarke <klayaclarke@gmail.com>
2021-11-18 09:54:06 -0500

    create methods that add to left or right side score and updates score after

--- score.py
+++ score.py
@@ -1,13 +1,30 @@
 from turtle import Turtle
 
-font = ('Arial', 16, 'normal')
+font = ('Arial', 70, 'normal')
 
 
 class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.penup()
-        self.score = 0
+        self.hideturtle()
+        self.color('white')
+        self.l_score = 0
+        self.r_score = 0
+        self.update_score()
 
-    def display_score(self):
-        self.write(arg=f'{self.score}', move=False, align='center', font=font, )
+    def update_score(self):
+        self.goto(x=-100, y=200)
+        self.write(self.l_score, align='center', font=font)
+        self.goto(x=100, y=200)
+        self.write(self.r_score, align='center', font=font)
+
+    def add_to_left_score(self):
+        self.l_score += 1
+        self.clear()
+        self.update_score()
+
+    def add_to_right_score(self):
+        self.r_score += 1
+        self.clear()
+        self.update_score()
